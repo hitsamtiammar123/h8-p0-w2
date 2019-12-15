498d488cc092c7bf7f3ee7dca81882bfa011a520
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-15 17:28:47 +0700

    mengubah format switch case jadi if else

--- exercise-3.js
+++ exercise-3.js
@@ -8,15 +8,14 @@ else if(peran==='')
 else{
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
     
-    switch(peran.toLowerCase().trim()){
-        case 'ksatria':
-            console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`)
-        break;
-        case 'tabib':
-            console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`)
-        break;
-        case 'penyihir':
-            console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
-        break;
+    var p=peran.toLowerCase().trim();
+    if(p==='ksatria'){
+        console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+    }
+    else if(p==='tabib'){
+        console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
+    }
+    else if(p==='penyihir'){
+        console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
     }
 }
