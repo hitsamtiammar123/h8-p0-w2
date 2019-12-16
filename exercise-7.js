a27165a821d0ca75d913df7272082f6ac5746eb1
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-16 10:51:50 +0700

    perubahan di exercise-7

--- exercise-7.js
+++ exercise-7.js
@@ -5,7 +5,6 @@ var rows1=10; // input the number of rows
 // do loops to display asterisks in the console.
 for(var i=0;i<rows1;i++){
     console.log("*");
-    console.log('');
 }
 
 //2. Menyusun Barisan Bintang Dengan Nested Looping
@@ -18,7 +17,6 @@ for(var i=0;i<rows2;i++){
     for(var j=0;j<rows2;j++)
         asterisks+='*';
     console.log(asterisks);
-    console.log('');
 }
 
 //3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
