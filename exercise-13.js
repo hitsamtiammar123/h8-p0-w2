f906c79a80dba15b6ac6475ee51f2d69fdb26a1b
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-16 11:33:14 +0700

    add exercise-13.js

---
+++ exercise-13.js
@@ -0,0 +1,24 @@
+function xo(str) {
+    // you can only write your code here!
+    str=str.toLowerCase();
+    var x=0;
+    var o=0;
+    for(var i=0;i<str.length;i++){
+        if(str[i]==='x')
+            x++;
+        else if(str[i]==='o')
+            o++;
+    }
+    if(x===o)
+        return true;
+    else 
+        return false;
+
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
