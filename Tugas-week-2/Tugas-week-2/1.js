de53f2737f750dc4107976e6163a4d3ad1763b4c
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-20 17:26:40 +0700

    add tugas liburan

---
+++ Tugas-week-2/Tugas-week-2/1.js
@@ -0,0 +1,52 @@
+/*
+------------
+LOST NUMBERS
+------------
+PROBLEM
+=======
+	lostNumbers adalah sebuah function yang akan menerima tiga parameter.
+	Tiga parameter tersebut adalah tiga angka yang berbeda.
+	Function akan mengembalikan setiap angka diantara angka pada tiga parameter tersebut, dari nilai parameter terkecil ke parameter terbesar
+	kecuali angka yang di input pada parameter
+Contoh:
+	first = 6, second = 1, third = 3
+	terbesar adalah 6, terkecil adalah 1,
+	maka function mengembalikan urutan angka secara berurutan seperti:
+	2, 4, 5
+
+Jika tidak ada angka yang ditampilkan, maka output "Tidak ada angka sama sekali".
+
+RULES
+=====
+- Wajib menggunakan pseudocode sebelum memulai kode
+*/
+
+//Pseudocode
+/**
+ * 
+ * 
+ * 
+ *
+ */
+
+function lostNumbers(first, second, third) {
+
+}
+
+console.log(lostNumbers(1, 3, 5));
+// // 2, 4
+
+console.log(lostNumbers(4, 6, 2));
+// // 3, 5
+
+console.log(lostNumbers(100, 108, 105));
+// // 101, 102, 103, 104, 106, 107
+
+console.log(lostNumbers(6, 5, 3));
+// // 4
+
+console.log(lostNumbers(3, 1, 2));
+// Tidak ada angka sama sekali
+
+console.log(lostNumbers(1, 1, 1));
+// Tidak ada angka sama sekali
