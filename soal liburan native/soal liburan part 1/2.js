7985857b7f95f6d9cab63b6f12023cdd0827c70e
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-20 22:22:43 +0700

    done for 2.js

--- soal liburan native/soal liburan part 1/2.js
+++ soal liburan native/soal liburan part 1/2.js
@@ -16,7 +16,7 @@
   cars ke - 0 = butuh waktu 2,5 jam
   cars ke - 1 = butuh waktu 2,25 jam
   cars ke - 2 = butuh waktu 2.35 jam
-  cars ke - 3 = butuh waktu 6.3 jam
+  cars ke - 3 = butuh waktu 0.625 jam
 
   [OUTPUT]
   [ 'B 1174 BA', 'B 1172 BA', 'B 1173 BA', 'B 1171 BA' ]
@@ -29,6 +29,28 @@
 
 function winner (cars, totalKM) {
   //code below here
+
+  for(var i=0;i<cars.length;i++){
+    var car=cars[i];
+    car.push(((totalKM-car[2])/car[1]).toPrecision(3));
+  }
+  
+  for(var i=0;i<cars.length;i++){
+    for(var j=0;j<cars.length-i-1;j++){
+      if(cars[j][3]>cars[j+1][3]){
+        var temp=cars[j];
+        cars[j]=cars[j+1];
+        cars[j+1]=temp;
+      }
+    }
+  }
+
+  var onlyPlatNo=[];
+  for(var i=0;i<cars.length;i++)
+    onlyPlatNo.push(cars[i][0]);
+
+  return onlyPlatNo;
+
 };
 
 console.log(winner([['B 1171 BA', 100, 200], ['B 1172 BA', 200, 0], ['B 1173 BA', 98, 220], ['B 1174 BA', 80, 400]], 450));
